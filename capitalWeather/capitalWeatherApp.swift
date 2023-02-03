dd5c8aae75e960d147ed987c8547a89e8f90587b
mouseorcleg <torindubozit@gmail.com>
2023-02-03 17:13:21 +0000

    Initial Commit

---
+++ capitalWeather/capitalWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  capitalWeatherApp.swift
+//  capitalWeather
+//
+//  Created by Maria Kharybina on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct capitalWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
